a381a0bedaee5e1bcebd28945c87c22af72a37d4
Marcossb125 <marcossbarja@gmail.com>
2026-05-10 19:41:26 +0200

    s

---
+++ calculatorSBM/calculatorSBM/src/main/java/com/dam/calculatorSBM/CalculatorSbmApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorSBM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSbmApplication.class, args);
+	}
+
+}
---
+++ calculatorSBM/calculatorSBM/src/test/java/com/dam/calculatorSBM/CalculatorSbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorSBM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
